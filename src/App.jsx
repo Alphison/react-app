eac02308c640962e95674beb50ced11d4fb1785f
Alphison <alfismusin13@gamil.com>
2023-01-13 14:40:51 +0300

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-import Header from './Header'
+import Header from './components/header/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
